0092b028d67664c7bc1003ee35c42b0a6193728b
PGCranford <PGCranford@gmail.com>
2022-06-10 14:59:21 -0400

    local storage work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
+
+//today vars
 var button = document.querySelector(".button")
 var inputCity = document.querySelector(".inputCity")
 var city = document.querySelector(".city")
 var temp = document.querySelector(".temp")
 var humidity = document.querySelector(".humidity")
 var windSpeed = document.querySelector(".windSpeed")
-var unIndex = document.querySelector(".uvIndex")
-var icon = document.querySelector("weatherIcon")
+var uvIndex = document.querySelector(".uvIndex")
+var showIcon = document.querySelector("weatherIcon")
+var today = document.querySelector("currentDay")
+var search = []
+
+//five day vars
+// var fiveDayDate = document.querySelector("dayContainer")
+// var fiveDaytemp = 
+// var fiveDayWind = 
+// var fiveDayHumidity = 
+
+
+var date =
+    moment().format("MM/DD/YY");
+console.log(date)
+// need to display 
 
 button.addEventListener("click", function () {
 
@@ -24,30 +40,60 @@ button.addEventListener("click", function () {
             windSpeed.innerHTML = windSpeedValue;
 
             console.log(data);
-        })
-        .catch(function () {
-            window.alert("Please enter a valid city")
-            // catch any errors
+
+            var iconId = data.weather[0].icon;
+            var iconUrl = " https://openweathermap.org/img/w/" + iconId + ".png"
+            dayIcon.setAttribute('src', iconUrl)
+
+
         });
+    // .catch (function (err) {
+    // window.alert("Please enter a valid city");
+
+    // });
+    localStorage.setItem("inputCity", JSON.stringify(inputCity))
+
+    //localStorage.setItem("inputCity", JSON.stringify(inputCity))
 
 
-
-    var = fiveDay = "api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid==b5500c263aef32c220366266b83504b5&units=imperial"
-    fetch(fiveDay)
-
-
-
-
-    //save searches to local storage
-
-    // var saveCity = localStorage.getItem(cityValue)
     // console.log(saveCity)
 
+    previousCities.addEventListener("click", function () {
+        let oldCity = inputCity.value;
+        localStorage.setItem(oldCity, JSON.stringify(inputCity));
+        if (previousCities) {
+            search = search + previousCities
+        }
+        //cities showing up as numbers not words 
+        console.log(search.length)
+        search.innerHTML = window.localStorage.getItem(search);
 
-    // localStorage.setItem((saveCity));
-    // console.log(localStorage)
 
-})
+    });
+
+
+});
+
+
+
+
+
+// var forecastFive = ("https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity.value + "&appid=b5500c263aef32c220366266b83504b5&units=imperial")
+// fetch(forecastFive)
+//     .then(function (response) { return response.json() }) // Convert data to json
+//     .then(function (data) {
+
+//         console.log(forecastFive);
+
+//         var setUpFiveDay function()
+//         // var fiveDayDate = data.list[0].dt_txt;
+//         // var fiveDaytemp = data.list[0]["main"[0]]
+//         // var fiveDayWind = data.list[0]["wind"]["speed"]
+//         // var fiveDayHumidity = data.list[0]["main"]["humidity"]
+
+
+
+//     }
 
 
 
